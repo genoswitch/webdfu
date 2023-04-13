c3e4cd4c480bf841f6126be251f2718e7fc26a9f
James Cahill <jcahill773@gmail.com>
2023-04-13 12:44:01 +0100

    (protocol/dfuse/command): Remove note about sector in spec (is actually mentioned)

--- src/protocol/dfuse/requests/command.ts
+++ src/protocol/dfuse/requests/command.ts
@@ -7,8 +7,6 @@
 export enum DfuSeRequestCommand {
 	GET_COMMAND = 0x00,
 	SET_ADDRESS_PTR = 0x21,
-	// Previous code calls this ERASE_SECTOR but I cannot find any mention of sectors in the spec.
-	// In 5.3 there are mentions of pages, however.
 	ERASE_SECTOR = 0x41,
 	// This command is not used by our code but is included for completeness.
 	READ_UNPROTECT = 0x92,
