a24c534f82adbc73dd2149371278cc62c725b513
James Cahill <jcahill773@gmail.com>
2023-04-23 17:02:27 +0100

    (parsers/usb): Create new configuration descriptor parser

--- src/parsers/usb/index.ts
+++ src/parsers/usb/index.ts
@@ -1,10 +1,12 @@
 import { descriptor } from "./descriptor";
+import { configurationDescriptor } from "./configurationDescriptor";
 import { deviceDescriptor } from "./deviceDescriptor";
 import { interfaceDescriptor } from "./interfaceDescriptor";
 import { subDescriptor } from "./subDescriptor";
 
 export default {
 	descriptor,
+	configurationDescriptor,
 	deviceDescriptor,
 	interfaceDescriptor,
 	subDescriptor,
