1f34d7981b813bf9a3407c645f5d7e3986d9fbc9
James Cahill <jcahill773@gmail.com>
2023-04-23 14:32:41 +0100

    (parsers/usb): Add subDescriptor to exports

--- src/parsers/usb/index.ts
+++ src/parsers/usb/index.ts
@@ -1,9 +1,11 @@
 import { descriptor } from "./descriptor";
 import { deviceDescriptor } from "./deviceDescriptor";
 import { interfaceDescriptor } from "./interfaceDescriptor";
+import { subDescriptor } from "./subDescriptor";
 
 export default {
 	descriptor,
 	deviceDescriptor,
 	interfaceDescriptor,
+	subDescriptor,
 };
