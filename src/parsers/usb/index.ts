6cde5bf494f96dbb0eadf353eeb60cf873739b64
James Cahill <jcahill773@gmail.com>
2023-04-25 13:14:07 +0100

    (types/usb): RefactorUSBDescriptor to be a class
    This commit changes the types/usb/descriptor.ts file to contain a class.
    
    Parser code has been moved to the constructor, with the previous parser function removed.

--- src/parsers/usb/index.ts
+++ src/parsers/usb/index.ts
@@ -1,11 +1,9 @@
-import { descriptor } from "./descriptor";
 import { configurationDescriptor } from "./configurationDescriptor";
 import { deviceDescriptor } from "./deviceDescriptor";
 import { interfaceDescriptor } from "./interfaceDescriptor";
 import { subDescriptor } from "./subDescriptor";
 
 export default {
-	descriptor,
 	configurationDescriptor,
 	deviceDescriptor,
 	interfaceDescriptor,
--- src/types/usb/descriptor.ts
+++ src/types/usb/descriptor.ts
@@ -5,21 +5,50 @@ import { USBDescriptorType } from "../../protocol/usb/descriptorTypes";
  *
  * This interface contains common properties used in all descriptors. The base interface, if you will.
  *
+ * @remarks
+ *
+ * Property sizes are taken from the USB Specification Revision 2, Section 9.6.1, Table 9-8. "Standard Device Descriptor".
+ *
  * https://www.beyondlogic.org/usbnutshell/usb5.shtml
  */
-export interface USBDescriptor {
+export class USBDescriptor {
 	/**
-	 * **Size of this descriptor in bytes**
+	 * Size of this descriptor in bytes.
+	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 0
+	 * @fieldByteType Uint8
 	 */
-	bLength: number;
+	readonly bLength: number;
 
 	/**
-	 *  **Interface Descriptor Type** (const)
+	 *  The Interface Descriptor Type of this descriptor.
+	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 1
+	 * @fieldByteType Uint8
 	 */
-	bDescriptorType: USBDescriptorType;
+	readonly bDescriptorType: USBDescriptorType;
 
 	/**
-	 * Raw data.
+	 * A `DataView` object containing the raw bytes that make up this descriptor.
 	 */
-	data?: DataView;
+	readonly data: DataView;
+
+	/**
+	 * Parse a USB (Base) Descriptor from the given `DataView`.
+	 *
+	 * This constructor will slice the DataView to only contain data from the same descriptor,
+	 * using the `bLength` variable. This DataView object is available under the `data` property.
+	 *
+	 * @param data The `DataView` object containing the descriptor data
+	 */
+	constructor(data: DataView) {
+		this.bLength = data.getUint8(0);
+		this.bDescriptorType = data.getUint8(1);
+
+		// Construct a DataView object with a subset of the data property corresponding to the descriptor size.
+		// This means that when this function is called with multiple descriptors, only the 'current' descriptor data is returned.
+		this.data = new DataView(data.buffer.slice(0, this.bLength));
+	}
 }
