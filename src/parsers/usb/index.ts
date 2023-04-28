d95ea4a395ad16a42255be57311d42b9f92b78f7
James Cahill <jcahill773@gmail.com>
2023-04-28 15:17:35 +0100

    (types/usb): refactor USBInterfaceDescriptor to be a class
    This commit changes the types/usb/interfaceDescriptor.ts file to contain a class.
    
    Parser code (formerly in src/parsers/usb/interfaceDescriptor.ts)
    has been moved to the constructor,
    with the previously mentioned parser file removed.

--- src/parsers/usb/index.ts
+++ src/parsers/usb/index.ts
@@ -1,11 +1,9 @@
 import { configurationDescriptor } from "./configurationDescriptor";
 import { deviceDescriptor } from "./deviceDescriptor";
-import { interfaceDescriptor } from "./interfaceDescriptor";
 import { subDescriptor } from "./subDescriptor";
 
 export default {
 	configurationDescriptor,
 	deviceDescriptor,
-	interfaceDescriptor,
 	subDescriptor,
 };
--- src/types/usb/interfaceDescriptor.ts
+++ src/types/usb/interfaceDescriptor.ts
@@ -3,58 +3,93 @@ import { USBDescriptor } from "./descriptor";
 
 /**
  * USB Interface Descriptor
- * Defined in the USB Specification Revision 2.0, Section 9.6.5 'Interface'
+ *
+ * @remarks
+ *
+ * The USB Interface Descriptor is defined in the USB Specification Revision 2.0, Section 9.6.5 'Interface'.
+ *
+ * Property sizes are taken from the USB Specification Revision 2, Section 9.6.5, Table 9-12. "Standard Interface Descriptor".
  *
  * Properties are annotated based on their respective descriptions from Table 9-12 in the same section.
  */
-export interface USBInterfaceDescriptor extends USBDescriptor {
+export class USBInterfaceDescriptor extends USBDescriptor {
 	/**
-	 * **Number of this interface.**
+	 * The number of this interface.
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 2
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * Zero-based value identifying the index in the array of concurrent interfaces
 	 * supported by this configuration.
 	 */
-	bInterfaceNumber: number;
+	readonly bInterfaceNumber: number;
 
 	/**
-	 * **Alternate setting for this interface.**
+	 * Alternate setting for this interface.
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 3
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * Value used to select an alternate setting for the interface specified in `bInterfaceNumber`.
 	 */
-	bAlternateSetting: number;
+	readonly bAlternateSetting: number;
 
 	/**
-	 * **Number of endpoints used by this interface (excluding endpoint zero).**
+	 * Number of endpoints used by this interface (excluding endpoint zero).
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 4
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * If this value is zero, this interface only uses the Default Control Pipe.
 	 */
-	bNumEndpoints: number;
+	readonly bNumEndpoints: number;
 
 	/**
-	 * **Class code (assigned by the USB-IF).**
+	 * Class code (assigned by the USB-IF).
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 5
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * The USB Class Codes are defined [here.](https://www.usb.org/defined-class-codes).
 	 *
 	 * `0xEF` is Miscellaneous (including DFU).
 	 * `0xFE` is Application-specific
 	 * `0xFF` is Vendor-specific
 	 */
-	bInterfaceClass: number;
+	readonly bInterfaceClass: number;
 
 	/**
-	 * **Subclass code (assigned by the USB-IF).**
+	 * Subclass code (assigned by the USB-IF).
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 6
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * These codes are qualified by the value of the `bDeviceClass` field.
 	 *
 	 * If the `bDeviceClass` field is reset to zero, this field must also be reset to zero.
 	 *
 	 * If the bDeviceClass field is not set to FFH, all values are reserved for assignment by the USB-IF.
 	 */
-	bInterfaceSubClass: number;
+	readonly bInterfaceSubClass: number;
 
 	/**
-	 * **Protocol code (assigned by the USB-IF)**
+	 * Protocol code (assigned by the USB-IF)
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 7
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * These codes are qualified by the value of the `bInterfaceClass` and the
 	 * `bInterfaceSubClass` fields. If an interface supports class-specific protocols
 	 * on a device basis as opposed to an interface basis, this code identifies
@@ -67,16 +102,43 @@ export interface USBInterfaceDescriptor extends USBDescriptor {
 	 * If this field is set to FFH, the device uses a vendor-specific protocol
 	 * on this interface.
 	 */
-	bInterfaceProtocol: number;
+	readonly bInterfaceProtocol: number;
 
 	/**
-	 * **Index of string descriptor describing this interface.**
+	 * Index of string descriptor describing this interface.
 	 *
+	 * @fieldByteSize 1
+	 * @fieldByteIndex 8
+	 * @fieldByteType Uint8
+	 *
+	 * @remarks
 	 * Similar to `iProduct` in the USB device descriptor.
 	 */
-	iInterface: number;
+	readonly iInterface: number;
 
 	// The existing webDFU code has these descriptor objects.
 	// This behaviour may be changed in the future but for now it will be ported across.
 	descriptors: (USBDescriptor | WebDFUInterfaceSubDescriptor)[];
+
+	/**
+	 * Parse a USB Interface Descriptor from the given `DataView`.
+	 *
+	 * @param data The `DataView` object containing the descriptor data
+	 */
+	constructor(data: DataView) {
+		super(data);
+
+		// bLength and bDescriptorType are set by the parent class.
+
+		this.bInterfaceNumber = this.data.getUint8(2);
+		this.bAlternateSetting = this.data.getUint8(3);
+		this.bNumEndpoints = this.data.getUint8(4);
+		this.bInterfaceClass = this.data.getUint8(5);
+		this.bInterfaceSubClass = this.data.getUint8(6);
+		this.bInterfaceProtocol = this.data.getUint8(7);
+		this.iInterface = this.data.getUint8(8);
+
+		// Initialize the `descriptors` variable as an empty array.
+		this.descriptors = [];
+	}
 }
