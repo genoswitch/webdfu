3cf6349434ed3227d50038b00503b1281f2c741c
James Cahill <jcahill773@gmail.com>
2023-04-30 12:32:44 +0100

    (util/interface): Add findInterface util function

---
+++ src/util/interface/find.ts
@@ -0,0 +1,23 @@
+import { InterfaceFilterPredicate } from "./filters";
+
+// Find an interface, assumming the device is already available
+export const findInterface = (
+	device: USBDevice,
+	predicate: InterfaceFilterPredicate
+): USBInterface => {
+	if (!device.configuration) {
+		throw new Error("Device configuration not available.");
+	}
+
+	const interfaces = device.configuration.interfaces;
+
+	const matches = interfaces.filter(predicate);
+
+	if (matches.length == 1) {
+		// TypeScript shows USBInterface[] indicies as USBInterface | undefined.
+		return matches[0] as USBInterface;
+	}
+
+	// Unexpected number of matches found
+	throw new Error("Unexpected number of matches found.");
+};
